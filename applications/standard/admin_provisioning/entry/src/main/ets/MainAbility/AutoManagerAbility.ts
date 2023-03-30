d1f8cc7ff1500d5c348582d63c1fee0ab92cf471
sorosyang <3410075870@qq.com>
2023-03-29 20:41:24 -0700

    admin_provisioning

---
+++ applications/standard/admin_provisioning/entry/src/main/ets/MainAbility/AutoManagerAbility.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIAbility from '@ohos.app.ability.UIAbility'
+import logger from '../common/logger'
+
+const TAG = 'AutoManagerAbility';
+
+export default class AutoManagerAbility extends UIAbility {
+    onCreate(want, launchParam) {
+        logger.info(TAG, 'onCreate')
+        globalThis.autoManagerAbilityWant = want;
+        globalThis.autoManagerAbilityContext = this.context;
+    }
+
+    onDestroy() {
+        logger.info(TAG, 'onDestroy')
+    }
+
+    onWindowStageCreate(windowStage) {
+        // Main window is created, set main page for this ability
+        logger.info(TAG, 'onWindowStageCreate')
+        windowStage.setUIContent(this.context, "pages/autoManager/managerStart", null)
+    }
+
+    onWindowStageDestroy() {
+        // Main window is destroyed, release UI related resources
+        logger.info(TAG, 'onWindowStageDestroy')
+    }
+
+    onForeground() {
+        // Ability has brought to foreground
+        logger.info(TAG, 'onForeground')
+    }
+
+    onBackground() {
+        // Ability has back to background
+        logger.info(TAG, 'onBackground')
+    }
+};
